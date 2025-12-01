81bf6448cecaa1d71b9390172da2e05830380b07
vlabiitk <vlabiitk@gmail.com>
2025-12-01 11:14:44 +0530

    simulation updated?

--- experiment/simulation/js/question.js
+++
@@ -1,8 +0,0 @@
- 
-const quiz = [
-  {
-  q:'On the basis of the calculated result analyse the flowability of your powder Sample',
-  options:['Excellent ','Good ','Fair','Passable','Poor','Very Poor','Very Very Poor'],
-  answer:3
-},
-]
--- experiment/simulation/script.js
+++ experiment/simulation/script.js
@@ -1,50 +1,105 @@
 
 let weight=0;
 let spatulaselect='';
-function startSimulation() {
+let powderselect=1;
+let checkstart=0;
+
+
+ function startSimulation(){
+  checkstart=1;
  weight=document.getElementById('weight').value;
  if(weight==0){
    alert("please enter weight");
     return;
  }
- document.getElementById('instruction').innerHTML='"Now click on powder cap"'
+ else{
+   if(checkonbtn==0){
+   document.getElementById('instruction').innerHTML='"Now click on weight machine ON button "'
+   }
+  else{
+    document.getElementById('instruction').innerHTML='"Now click on box Cap "'
+  }
+ }
 
- let powderselect=document.getElementById('powder').value;
-  
+  powderselect=document.getElementById('powder').value;
    switch(powderselect){
     case "1":
-      document.getElementById('Nacl').classList.toggle('opencap');
+      // document.getElementById('Nacl').classList.toggle('opencap');
       spatulaselect='spatulamove_1';
       break;
     case "2":
-      document.getElementById('Nacltalc').classList.toggle('opencap');
+      // document.getElementById('Nacltalc').classList.toggle('opencap');
       spatulaselect='spatulamove_2';
       break;
    }
   }
-  
 
-  function handleonclick(el){
-      document.getElementById('instruction').innerHTML='"Now click on spatula"'
-      el.classList.toggle('opencap');
-      
+   let checkonbtn=0;
+  function ONBUTTON(){
+     checkonbtn=1;
+     document.getElementById('weightcounter').innerHTML="<span style='color:red'>0.00 gm <span/>";
+     if(checkstart>0  && checkonbtn>0){
+     document.getElementById('instruction').innerHTML='"Now click on box Cap "'
+     }
+     else if(checkonbtn>0){
+         document.getElementById('instruction').innerHTML='" Please Enter weight and select powder and click on start"'
+     }
+     else{
+       document.getElementById('instruction').innerHTML='" please click on weight machine ON button"'
+     }
+  }
+
+  function TAREBUTTON(){
+    checkonbtn=0;
+    document.getElementById('weightcounter').innerHTML="<span>0.00 gm <span/>";
+     document.getElementById('instruction').innerHTML='" Now click on weight machine ON button"'
+  }
+
+
+   let opencapcheck=0;
+
+   function handleonclick(el,id){
+    if(checkstart==0){
+      alert('please start the simulater');
+    }
+    else{
+     if(id==powderselect){
+        el.classList.toggle('opencap');
+        setTimeout(function() {
+             document.getElementById('instruction').innerHTML='"Now click on spatula"'          
+              opencapcheck++;
+        },2000);
+     
+     }
+     else{
+      alert('please click on selected powder ')
+     }
+    }
   }
 
    document.getElementById('powderid1').classList.toggle('box')
+   let checkspatula=0;
+
 
   function spatulaonclick(el){
+    if(opencapcheck==0){
+      alert('please open cap');
+    }
+    else
+      {
     // animateWeight(100);
-    document.getElementById('instruction').innerHTML='"Now click on petridish"';
     // document.getElementById('spatu').classList.toggle('spatulamove');
     // document.getElementById('powd').classList.toggle('powderspatula');
      el.classList.toggle(`${spatulaselect}`)
     if(weight==0){
-      document.getElementById('weightcounter').innerHTML='0.00 gm';
+      document.getElementById('weightcounter').innerHTML="<span style='color:red'>0.00 gm <span/>";
     }
     setTimeout(function() {
        document.getElementById('powderid1').classList.toggle('powder');
        document.getElementById('powderid1').classList.remove('box');
        animateWeight(weight);
+         checkspatula++;
+         document.getElementById('instruction').innerHTML='"Now click on petridish"';
     },6000);
 
      setTimeout(function() {
@@ -55,16 +110,20 @@ function startSimulation() {
       document.getElementById('powd').style.opacity=0;
    },5600);
     
-   }
+   }}
 
    function petridishmove() {
+    if(checkspatula==0){
+      alert('please put the powder');
+    }
+    else{
     document.getElementById('petridishmove1').classList.toggle('petridishmove');
     document.getElementById('powderid1').classList.toggle('petridishpowdermove'); 
     setTimeout(() => {
       document.getElementById("overlay").style.display = "flex";
       calculateRepose(weight);
     }, 8000);
-  }
+  }}
   
 
   function animateWeight(target) {
@@ -75,6 +134,7 @@ function startSimulation() {
     const interval = setInterval(() => {
       current += step;
       display.textContent = current.toFixed(2) + "g";
+      display.style.color = "red"; 
       count++;
       if (count >= 20) clearInterval(interval);
     },100);
